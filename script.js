13605c1fd0d0af71fbdbbd3c62d5136d3e27aba8
Thais Mariano <86856151+thaisdevfront@users.noreply.github.com>
2026-02-14 00:39:44 -0300

    up - completo

--- script.js
+++ script.js
@@ -103,3 +103,13 @@ window.addEventListener('resize', () => {
   index = 0;
   track.style.transform = `translateX(0)`;
 });
+
+// whatsapp
+function abrirWhatsApp() {
+const telefone = "5512981021517"; // DDI + DDD + número
+const mensagem = "Olá! Gostaria de mais informações";
+
+
+const url = `https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`;
+window.open(url, "_blank");
+}
