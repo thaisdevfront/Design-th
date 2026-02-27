0398c1a658ff3fbfee4b290b69811e58b6602571
Thais Mariano <86856151+thaisdevfront@users.noreply.github.com>
2026-02-26 21:28:04 -0300

    up - assets

---
+++ assets/js/script.js
@@ -0,0 +1,161 @@
+ const items = document.querySelectorAll(".scroll-item");
+
+function handleScroll() {
+  const triggerBottom = window.innerHeight * 0.95;
+  const triggerTop = window.innerHeight * 0.2;
+
+  items.forEach(item => {
+    const box = item.getBoundingClientRect();
+
+    // ENTRADA
+    if (box.top < triggerBottom && box.bottom > triggerTop) {
+      item.classList.add("show");
+      item.classList.remove("hide");
+    }
+
+    // SAÍDA
+    else if (box.top <= triggerTop) {
+      item.classList.remove("show");
+      item.classList.add("hide");
+    }
+
+    else {
+      item.classList.remove("show", "hide");
+    }
+  });
+}
+
+window.addEventListener("scroll", handleScroll);
+handleScroll();
+
+//whatsapp
+function abrirWhatsApp() {
+const telefone = "5512981021517"; // DDI + DDD + número
+const mensagem = "Olá! Gostaria de mais informações";
+
+
+const url = `https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`;
+window.open(url, "_blank");
+}
+
+
+
+
+// naav
+const header = document.getElementById("header");
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+
+// scroll header
+window.addEventListener("scroll", () => {
+  header.classList.toggle("scrolled", window.scrollY > 80);
+});
+
+// abrir / fechar menu
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("open");
+});
+
+// fechar menu ao clicar em um link
+menu.querySelectorAll("a").forEach(link => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("open");
+  });
+});
+
+
+
+// portfólio
+document.addEventListener('DOMContentLoaded', () => {
+
+  // ELEMENTOS
+ const buttons = document.querySelectorAll('.filters button');
+const cards = document.querySelectorAll('.card');
+
+
+  const modal = document.getElementById('modal');
+  const modalImage = document.getElementById('modalImage');
+  const modalTitle = document.getElementById('modalTitle');
+  const modalDescription = document.getElementById('modalDescription');
+  const modalElements = document.getElementById('modalElements');
+    const modalCategory = document.getElementById('modalCategory');
+  const closeBtn = document.querySelector('.close');
+
+  /* ==========================
+     FILTRO
+  ========================== */
+  buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    buttons.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+
+    const filter = button.dataset.filter;
+
+    cards.forEach(card => {
+      card.style.display =
+        filter === 'all' || card.classList.contains(filter)
+          ? 'block'
+          : 'none';
+    });
+  });
+});
+
+  /* ==========================
+     MODAL
+  ========================== */
+ 
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    modal.style.display = 'flex';
+
+    modalImage.src = card.querySelector('img').src;
+    modalTitle.textContent = card.dataset.title;
+    modalDescription.textContent = card.dataset.description;
+    modalElements.textContent = card.dataset.elements;
+    modalCategory.textContent = card.dataset.category;
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+modal.addEventListener('click', e => {
+  if (e.target === modal) modal.style.display = 'none';
+});
+
+
+});
+
+
+// lógica do “Ver mais”
+const btnMais = document.querySelector(".btn-mais");
+const maisPort = document.querySelector(".maisport");
+
+btnMais.addEventListener("click", () => {
+  maisPort.classList.toggle("aberto");
+
+  btnMais.innerText = maisPort.classList.contains("aberto")
+    ? "Ver menos"
+    : "Ver mais";
+});
+
+
+// Bloquear tecla Print Screen
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'PrintScreen') {
+    e.preventDefault();
+    alert('Captura de tela bloqueada');
+  }
+});
+
+// Bloquear Ctrl + P
+document.addEventListener('keydown', function (e) {
+  if (e.ctrlKey && e.key === 'p') {
+    e.preventDefault();
+    alert('Impressão desativada');
+  }
+});
+
+// Desativar botão direito (copiar / salvar)
+document.addEventListener('contextmenu', e => e.preventDefault());
